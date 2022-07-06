cf968a81eacc1d5200ee08910e5464b08927d34b
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-07-06 17:04:56 -0300

    dockerfile-e-migrations

---
+++ app/backend/src/database/migrations/user.js
@@ -0,0 +1,27 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('users', {
+            id:{
+                primaryKey: true,
+                autoIncrement: true,
+                type: Sequelize.INTEGER,
+            },
+            user_name: {
+                type: Sequelize.STRING
+            },
+            role: {
+                type: Sequelize.STRING
+            },
+            email: {
+                type: Sequelize.STRING
+            },
+            password: {
+                type: Sequelize.STRING
+            },
+        });
+    },
+
+    down: async (queryInterface) => {
+        await queryInterface.dropTable('users');
+    },
+};
