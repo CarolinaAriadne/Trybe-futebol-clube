34b6587f4a280bc7a5f04532e429f33bf85f5317
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-07-08 16:44:01 -0300

    Testes-reqs-6-8

--- app/backend/src/tests/Users.test.ts
+++ app/backend/src/tests/Users.test.ts
@@ -5,6 +5,7 @@ import { before } from 'mocha';
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Users from '../database/models/Users';
+import * as jwt from 'jsonwebtoken';
 
 chai.use(chaiHttp);
 
@@ -23,14 +24,16 @@ describe('Users', () => {
           role: 'fone',
           email: 'seiquela@seiquela.com',
           password: '1234567',
-        } as Users);
+        } as Users)
+        sinon.stub(jwt, 'sign')
+        .resolves('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc')
+        
     });
     after(() => {
       (Users.findOne as sinon.SinonStub).restore();
+      (jwt.sign as sinon.SinonStub).restore();
     });
     it('Success request to POST /login', async () => {
-
-      let token = {'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImpvZWxobyIsInJvbGUiOiJmb25lIiwiZW1haWwiOiJzZWlxdWVsYUBzZWlxdWVsYS5jb20iLCJwYXNzd29yZCI6IjEyMzQ1NjcifSwiaWF0IjoxNjU3MzA3MjI3LCJleHAiOjE2NjExOTUyMjd9.nyOGO8lf12YxYy2RM1NDUnRGH1bm9saBVq38twOGcGM'}
 
       chaiHttpResponse = await chai
         .request(app)
@@ -38,7 +41,7 @@ describe('Users', () => {
         .send({ email: 'seiquela@seiquela.com', password: '1234567' });
 
       expect(chaiHttpResponse.status).to.be.equal(200);
-          expect(chaiHttpResponse.body).to.be.eql(token);
+          expect(chaiHttpResponse.body).to.be.eql({token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'});
     });
     it('Bad request - email empty', async () => {
       chaiHttpResponse = await chai 
