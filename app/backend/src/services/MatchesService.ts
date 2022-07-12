f4cfb15b38cfb4ad0a48fe00af6c7c6e78c21a1d
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-07-12 13:06:40 -0300

    Reqs-25-26-incompletos

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -11,31 +11,44 @@ export default class MatcheService {
       {model: Team, as: 'teamHome', attributes:{exclude: ['id']}}, 
       {model: Team, as:'teamAway', attributes:{exclude: ['id']}},
   ]});
-    return matches;
+      return matches;
   };
- createMatcheService = async (matche: IMatche) => {
+  createMatcheService = async (matche: IMatche) => {
 
-    const {homeTeam, homeTeamGoals, awayTeam, awayTeamGoals} = matche;
+    // const matcheHome = await Matches.findOne( {where:
+    //   {id: 'homeTeam'}});
+    //   const matcheAway = await Matches.findOne({where:
+    //   {id: 'awayTeam'}});
 
-    const matches = await Matches.create(
+    //   if(matcheHome === matcheAway){
+    //     throw new CustomError(401, 'It is not possible to create a match with two equal teams!');
+    //   }
+
+    //   if(!matcheHome || !matcheAway){
+    //     throw new CustomError(404, 'There is no team with such id!');
+    //   }
+
+      const {homeTeam, homeTeamGoals, awayTeam, awayTeamGoals} = matche;
+
+      const matches = await Matches.create(
     
-    {homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress: true},
+      {homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress: true},
   );
   // console.log(matches, 'matches')
 
-    return matches;
+      return matches;
   };
 
   updateStatusMatche = async (id: Identifier | undefined) => {
     const matcheId = await Matches.findByPk(id);
 
-    if (!matcheId) {
+      if (!matcheId) {
       throw new CustomError(404, 'Matche does not exist');
-    }
+      }
 
-    await Matches.update({inProgress: false}, {where: {id}});
+      await Matches.update({inProgress: false}, {where: {id}});
 
-    return {message: 'Finished'};
+      return {message: 'Finished'};
 
   }
 };
