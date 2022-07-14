b448d8edaf8d981792a32e6418fabc8ecc7a9f43
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-07-14 19:40:27 -0300

    Req-29-incompleto

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,60 +1,45 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
 // // import CustomError from '../interfaces/custom.error';
 // import IMatche from '../interfaces/Matches';
 // import ILeaderBoard from '../interfaces/Leaderboard';
 import Teams from '../database/models/Teams';
 // import ITeams from '../interfaces/Teams';
 import Matches from '../database/models/Matches';
+import { goalsFavor, goalsOwn, totalPoints, totalGames,
+  totalVictores, totalDraws, totalLosses } from '../utils/functions';
+// import MatcheService from './MatchesService';
 
 export default class LeatherBoardService {
-  private teams: Teams[];
-  private matches: Matches[];
-  private matchesByPk: Matches [];
-  // totalPoints: number;
-  // totalGames: number;
-  // totalVictories: number; // caso a interface seja implementada
-  // totalDraws: number;
-  // totalLosses: number;
-  // goalsFavor: number;
-  // goalsOwn: number;
-  // goalsBalance: number;
-  // efficiency: string;
+  // eslint-disable-next-line max-lines-per-function
   public homeTeamRankingService = async () => {
-    // const quantifyGoals = await Matches.findAll({ attributes: { include: ['homeTeamGoals',
-    //   'awayTeamGoals'],
-    // exclude: ['id', 'homeTeam', 'awayTeam', 'inProgress'] },
-    // where: { inProgress: false } });
-    this.teams = await Teams.findAll();
-    this.matches = await Matches.findAll();
-    this.matchesByPk = await Matches.findByPk();
+    const matches = await Teams.findAll(
+      { include: [
+        { model: Matches,
+          as: 'matchesHome',
+          where: { inProgress: false },
+        },
+      ] },
+    );
 
-    // const totalVictories: any = quantifyGoals.reduce((accumulator: any, actualValue: any) => {
-    //   if (actualValue.homeTeamGoals > actualValue.awayTeamGoals) {
-    //     return accumulator + 3;
-    //   }
+    const allTeams = matches.map((team:any) => {
+      const totalPointsFunc = totalPoints(team.matchesHome);
+      const totalPointsGames = totalGames(team.matchesHome);
 
-    //   return totalVictories;
-    // }, 0);
-
-    // const totalVictories = () => {
-    //   const totalVictoriesHome = quantifyGoals.reduce((previousValue, contador) => {
+      return {
+        name: team.teamName,
+        totalPoints: totalPoints(team.matchesHome),
+        totalGames: totalGames(team.matchesHome),
+        totalVictories: totalVictores(team.matchesHome),
+        totalDraws: totalDraws(team.matchesHome),
+        totalLosses: totalLosses(team.matchesHome),
+        goalsFavor: goalsFavor(team.matchesHome),
+        goalsOwn: goalsOwn(team.matchesHome),
+        totalBalance: goalsFavor(team.matchesHome) - goalsOwn(team.matchesHome),
+        efficiency: Number(((((totalPointsFunc) / (totalPointsGames * 3)) * 100)).toFixed(2)),
+      };
+    });
+    return allTeams;
   };
-
-  public totalVictories = (matche: Matches[]): number => matche.reduce((acc, actual) => {
-    if (actual.homeTeamGoals > actual.awayTeamGoals) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-
-  public totalDraws = (matche: Matches[]): number => matche.reduce((acc, actual) => {
-    if (actual.homeTeamGoals === actual.awayTeamGoals) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-
-  public totalPoints = (matche: Matches[]) => this.totalVictories(matche)
-  * 3 + this.totalDraws(matche);
-
-  public totalGames = (matche: Matches[]) => matche.length;
 }
---
+++ app/backend/src/utils/functions.ts
@@ -0,0 +1,47 @@
+function goalsFavor(matchesHome:any) {
+  return matchesHome.reduce((acc:any, curr:any) => acc + curr.homeTeamGoals, 0);
+}
+
+function goalsOwn(matchesHome:any) {
+  return matchesHome.reduce((acc:any, curr:any) => acc + curr.awayTeamGoals, 0);
+}
+
+function totalPoints(matchesHome:any) {
+  return matchesHome.reduce((acc:any, curr:any) => {
+    let contador = acc;
+    // console.log(JSON.parse(JSON.stringify(team)));
+    if (curr.homeTeamGoals > curr.awayTeamGoals) contador += 3;
+    if (curr.homeTeamGoals === curr.awayTeamGoals) contador += 1;
+    return contador;
+  }, 0);
+}
+
+function totalGames(matchesHome:any) {
+  return matchesHome.length;
+}
+
+function totalVictores(matchesHome:any) {
+  return matchesHome.reduce((acc:any, curr:any) => {
+    let contador = acc;
+    if (curr.homeTeamGoals > curr.awayTeamGoals) contador += 1;
+    return contador;
+  }, 0);
+}
+
+function totalDraws(matchesHome:any) {
+  return matchesHome.reduce((acc:any, curr:any) => {
+    let contador = acc;
+    if (curr.homeTeamGoals === curr.awayTeamGoals) contador += 1;
+    return contador;
+  }, 0);
+}
+
+function totalLosses(matchesHome:any) {
+  return matchesHome.reduce((acc:any, curr:any) => {
+    let contador = acc;
+    if (curr.homeTeamGoals < curr.awayTeamGoals) contador += 1;
+    return contador;
+  }, 0);
+}
+
+export { goalsFavor, goalsOwn, totalPoints, totalGames, totalVictores, totalDraws, totalLosses };
